05a9335d6a4f5b5f61897d6a5efeb08a432bb788
khoaluu2014 <khoaluu2014@gmail.com>
2023-11-08 21:30:12 -0800

    finished

--- js/site.js
+++ js/site.js
@@ -1,38 +1,46 @@
-loadHolidays();
+document.addEventListener("DOMContentLoaded", () => {
+	const form = document.getElementById("holidaysForm");
 
-async function loadHolidays() {
-	document
-		.getElementById("holidaysForm")
-		.addEventListener("submit", async function (event) {
-			// Prevent the default form submission
-			event.preventDefault();
+	form.addEventListener("submit", async function (event) {
+		event.preventDefault(); // Prevent form from submitting and refreshing the page
 
-			const year = document.getElementById("txtYear").value;
-			const countryCode = document.getElementById("txtCountryCode").value;
-			const tbody = document.getElementById("holidaysTable");
+		const year = document.getElementById("txtYear").value;
+		const countryCode = document.getElementById("txtCountryCode").value;
+		const tbody = document.getElementById("holidaysTable");
 
-			try {
-				const holidays = await getHolidays(year, countryCode);
+		// Debugging line: Check if this gets printed every time you click submit
+		console.log("Form submitted with: ", year, countryCode);
 
-				// Clear previous results
-				tbody.innerHTML = "";
+		try {
+			// Clear previous results
+			tbody.innerHTML = "";
 
-				// Add new results to the table body
+			const holidays = await getHolidays(year, countryCode);
+			if (holidays && holidays.length) {
+				// Append new results to the table
 				holidays.forEach((holiday) => {
 					let row = `<tr>
-								<td>${holiday.date}</td>
-								<td>${holiday.name}</td>
-								<td>${holiday.localName}</td>
-								<td>${holiday.countryCode}</td>
-								<td>${holiday.global ? "Yes" : "No"}</td>
-						   </tr>`;
-					tbody.innerHTML += row;
+                          <td>${holiday.date}</td>
+                          <td>${holiday.name}</td>
+                          <td>${holiday.localName}</td>
+                          <td>${holiday.countryCode}</td>
+                          <td>${holiday.global ? "Yes" : "No"}</td>
+                        </tr>`;
+					tbody.innerHTML += row; // Consider using `insertAdjacentHTML` for better performance
 				});
-			} catch (error) {
-				console.error("Error fetching holidays:", error);
+			} else {
+				// No holidays found or an error occurred
+				tbody.innerHTML =
+					'<tr><td colspan="5">No holidays found or an error occurred.</td></tr>';
 			}
-		});
-}
+		} catch (error) {
+			console.error("Error fetching holidays:", error);
+			tbody.innerHTML =
+				'<tr><td colspan="5">Error loading holidays. Please try again.</td></tr>';
+		}
+	});
+});
+
 async function getHolidays(year, countryCode) {
 	// if (!countryCode || year <= 0) {
 	// 	return []; // Return an empty array if the inputs are not valid.
@@ -40,7 +48,7 @@ async function getHolidays(year, countryCode) {
 
 	try {
 		const response = await fetch(
-			`https://date.nager.at/api/v2/PublicHolidays/1999/US`
+			`https://date.nager.at/api/v2/PublicHolidays/${year}/${countryCode}`
 		);
 		if (!response.ok) {
 			throw new Error(`HTTP error! status: ${response.status}`);
